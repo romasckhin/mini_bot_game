242de47869960f9171ab1aa468d7a5bf9034c5e8
Gaming <cikipuki10@gmail.com>
2021-08-25 20:58:08 +0300

    new commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const readlineSync = require('readline-sync');
 const chalk = require('chalk');
-const {Quiz} = require('enquirer');
+const inquirer = require('inquirer');
+
 console.log('       ====================================================================')
 console.log(chalk.magenta(' -- Hello lucky guy, you have the opportunity to win cool prizes  are you ready? -- '))
 console.log('       ====================================================================')
@@ -13,8 +14,8 @@ const obj = {
 }
 if (readlineSync.keyInYN(chalk.green(' Try to Register? '))) {
 
-    obj.name = readlineSync.question(chalk.gray.underline(' What your name?: ')),
-        obj.wallet = readlineSync.question(chalk.gray.underline(' Account balance: ')),
+    obj.name = readlineSync.question(chalk.gray.underline(' What your name?: '))
+        obj.wallet = readlineSync.question(chalk.gray.underline(' Account balance: '))
         obj.password = readlineSync.question(chalk.gray.underline(' Your password?: '))
 
 
@@ -29,88 +30,202 @@ if (obj.name.length === 0 || obj.password.length === 0 || obj.wallet.length === 
     console.log(chalk.black.bold.bgYellow(`\n Your Account `), ` \n { name: ${obj.name}, wallet: ${obj.wallet}, password: ${obj.password} }`)
     console.log('\n')
 
+    console.log('Choose a task. ')
 
-    // let question = readlineSync.question(` Choose the difficult of the level: (${chalk.gray('easy')},${chalk.green('medium')},${chalk.red('hard')}): `)
 
     const readlineSync = require('readline-sync'),
-        animals = [chalk.gray('easy'),chalk.green('medium'),chalk.red('hard'),],
+        animals = [chalk.gray('easy'), chalk.green('medium'), chalk.red('hard'),],
         index = readlineSync.keyInSelect(animals, 'Choose the difficult of the level: ');
 
-    if ( index === 0 ) {
+    if (index === 0) {
 
-        const prompt = new Quiz({
-            name: 'JavaScript',
-            message: 'What type of data is does not exit in JavaScript? ',
-            choices: ['boolean', 'indexof', 'number', 'object'],
-            correctChoice: 1
-        });
+        function one() {
+            inquirer.prompt(
+                [
+                    {
+                        type: 'list',
+                        name: 'answersOne',
+                        message: 'What type of data is does not exit in JavaScript? ',
+                        choices: ['boolean', 'indexof', 'number', 'object'],
+                    },
+                    {
+                        type: 'list',
+                        name: 'answersTwo',
+                        message: 'How else can the ternary operator be called ? ',
+                        choices: ['switch', 'if else', 'question mark', 'point'],
+                    },
+                    {
+                        type: 'list',
+                        name: 'answersThree',
+                        message: 'Find the wrong string method ',
+                        choices: ['toLowerCase', 'startsWith', 'indexof', 'includes'],
+                    },
+                ]
+            )
+                .then(answer => {
+                    let w = 0;
+                    let countOne = 0;
+                    let arr = answer
+                    if (arr['answersOne'] === 'indexof') {
+                        console.log(`${chalk.blue(arr['answersOne'])}: ${chalk.green('The answer is correct!')}`)
+                        w += 10
+                        countOne++
+                    } else {
+                        console.log(`${chalk.blue(arr['answersOne'])}: ${chalk.red('The answer is incorrect!')}`)
+                    }
+                    if (arr['answersTwo'] === 'question mark') {
+                        console.log(`${chalk.blue(arr['answersTwo'])}: ${chalk.green('The answer is correct!')}`)
+                        w += 10
+                        countOne++
+                    } else {
+                        console.log(`${chalk.blue(arr['answersTwo'])}: ${chalk.red('The answer is incorrect!')}`)
+                    }
+                    if (arr['answersThree'] === 'indexof') {
+                        console.log(`${chalk.blue(arr['answersThree'])}: ${chalk.green('The answer is correct!')}`)
+                        w += 10
+                        countOne++
+                    } else {
+                        console.log(`${chalk.blue(arr['answersThree'])}: ${chalk.red('The answer is incorrect!')}`)
+                    }
+                    if (countOne < 3) {
+                        one()
+                    }
+                    if (countOne === 3) {
+                        console.log(chalk.green(`Bonus + ${w}:`), '{wallet:', `${+obj.wallet + w}}`)
+                        console.log(chalk.black.bold.bgYellow(`\n Your Account `), ` \n { name: ${obj.name}, wallet: ${+obj.wallet + w}, password: ${obj.password} }`)
 
-        prompt
-            .run()
-            .then(answer => {
-                if (answer.correct) {
-                    console.log('Correct!');
-                    let bonus = 50;
-                    console.log(chalk.green('Bonus + 50$:'),'{wallet:',`${+obj.wallet + bonus}}`)
-                    console.log(chalk.black.bold.bgYellow(`\n Your Account `), ` \n { name: ${obj.name}, wallet: ${+obj.wallet + bonus}, password: ${obj.password} }`)
-                } else {
-                    console.log(`Wrong! Correct answer is ${answer.correctAnswer}`);
-                    let loss = 50;
-                    console.log(chalk.red('Loss - 50$:'),'{wallet:',`${+obj.wallet - loss}}`)
-                    console.log(chalk.black.bold.bgYellow(`\n Your Account `), ` \n { name: ${obj.name}, wallet: ${+obj.wallet - loss}, password: ${obj.password}}`)
-                }
-            })
-            .catch(console.error);
+                    }
+                })
+        }
+
+        one()
 
     }
-    if ( index === 1 ) {
-        const prompt = new Quiz({
-            name: 'JavaScript',
-            message: 'How else can the ternary operator be called ? ',
-            choices: ['switch', 'if else', 'question mark', 'point'],
-            correctChoice: 2
-        });
+    if (index === 1) {
 
-        prompt
-            .run()
-            .then(answer => {
-                if (answer.correct) {
-                    console.log('Correct!');
-                    let bonus = 100;
-                    console.log(chalk.green('Bonus + 100$:'),'{wallet:',`${+obj.wallet + bonus}}`)
-                    console.log(chalk.black.bold.bgYellow(`\n Your Account `), ` \n { name: ${obj.name}, wallet: ${+obj.wallet + bonus}, password: ${obj.password} }`)
-                } else {
-                    console.log(`Wrong! Correct answer is ${answer.correctAnswer}`);
-                    let loss = 100;
-                    console.log(chalk.red('Loss - 100$:'),'{wallet:',`${+obj.wallet - loss}}`)
-                    console.log(chalk.black.bold.bgYellow(`\n Your Account `), ` \n { name: ${obj.name}, wallet: ${+obj.wallet - loss}, password: ${obj.password}}`)
-                }
-            })
-            .catch(console.error);
+        function two() {
+            inquirer.prompt(
+                [
+                    {
+                        type: 'list',
+                        name: 'answersOne',
+                        message: 'What type of data is does not exit in JavaScript? ',
+                        choices: ['boolean', 'indexof', 'number', 'object'],
+                    },
+                    {
+                        type: 'list',
+                        name: 'answersTwo',
+                        message: 'How else can the ternary operator be called ? ',
+                        choices: ['switch', 'if else', 'question mark', 'point'],
+                    },
+                    {
+                        type: 'list',
+                        name: 'answersThree',
+                        message: 'Find the wrong string method ',
+                        choices: ['toLowerCase', 'startsWith', 'indexof', 'includes'],
+                    },
+                ]
+            )
+                .then(answer => {
+                    let w = 0;
+                    let countTwo = 0;
+                    let arr = answer
+                    if (arr['answersOne'] === 'indexof') {
+                        console.log(`${chalk.blue(arr['answersOne'])}: ${chalk.green('The answer is correct!')}`)
+                        w += 50
+                        countTwo++
+                    } else {
+                        console.log(`${chalk.blue(arr['answersOne'])}: ${chalk.red('The answer is incorrect!')}`)
+                    }
+                    if (arr['answersTwo'] === 'question mark') {
+                        console.log(`${chalk.blue(arr['answersTwo'])}: ${chalk.green('The answer is correct!')}`)
+                        w += 50
+                        countTwo++
+                    } else {
+                        console.log(`${chalk.blue(arr['answersTwo'])}: ${chalk.red('The answer is incorrect!')}`)
+                    }
+                    if (arr['answersThree'] === 'indexof') {
+                        console.log(`${chalk.blue(arr['answersThree'])}: ${chalk.green('The answer is correct!')}`)
+                        w += 50
+                        countTwo++
+                    } else {
+                        console.log(`${chalk.blue(arr['answersThree'])}: ${chalk.red('The answer is incorrect!')}`)
+                    }
+                    if (countTwo < 3) {
+                        two()
+                    }
+                    if (countTwo === 3) {
+                        console.log(chalk.green(`Bonus + ${w}:`), '{wallet:', `${+obj.wallet + w}}`)
+                        console.log(chalk.black.bold.bgYellow(`\n Your Account `), ` \n { name: ${obj.name}, wallet: ${+obj.wallet + w}, password: ${obj.password} }`)
+                    }
+                })
+        }
+
+        two()
+
     }
-    if ( index === 2 ) {
-        const prompt = new Quiz({
-            name: 'JavaScript',
-            message: 'Find the wrong string method ',
-            choices: ['toLowerCase', 'startsWith', 'indexof', 'includes'],
-            correctChoice: 2
-        });
+    if (index === 2) {
 
-        prompt
-            .run()
-            .then(answer => {
-                if (answer.correct) {
-                    console.log('Correct!');
-                    let bonus = 200;
-                    console.log(chalk.green('Bonus + 200$:'),'{wallet:',`${+obj.wallet + bonus}}`)
-                    console.log(chalk.black.bold.bgYellow(`\n Your Account `), ` \n { name: ${obj.name}, wallet: ${+obj.wallet + bonus}, password: ${obj.password} }`)
-                } else {
-                    console.log(`Wrong! Correct answer is ${answer.correctAnswer}`);
-                    let loss = 200;
-                    console.log(chalk.red('Loss - 200$:'),'{wallet:',`${+obj.wallet - loss}}`)
-                    console.log(chalk.black.bold.bgYellow(`\n Your Account `), ` \n { name: ${obj.name}, wallet: ${+obj.wallet - loss}, password: ${obj.password}}`)
-                }
-            })
-            .catch(console.error);
+        function three() {
+            inquirer.prompt(
+                [
+                    {
+                        type: 'list',
+                        name: 'answersOne',
+                        message: 'What type of data is does not exit in JavaScript? ',
+                        choices: ['boolean', 'indexof', 'number', 'object'],
+                    },
+                    {
+                        type: 'list',
+                        name: 'answersTwo',
+                        message: 'How else can the ternary operator be called ? ',
+                        choices: ['switch', 'if else', 'question mark', 'point'],
+                    },
+                    {
+                        type: 'list',
+                        name: 'answersThree',
+                        message: 'Find the wrong string method ',
+                        choices: ['toLowerCase', 'startsWith', 'indexof', 'includes'],
+                    },
+                ]
+            )
+                .then(answer => {
+                    let w = 0;
+                    let countThree = 0;
+                    let arr = answer
+                    if (arr['answersOne'] === 'indexof') {
+                        console.log(`${chalk.blue(arr['answersOne'])}: ${chalk.green('The answer is correct!')}`)
+                        w += 100
+                        countThree++
+                    } else {
+                        console.log(`${chalk.blue(arr['answersOne'])}: ${chalk.red('The answer is incorrect!')}`)
+                    }
+                    if (arr['answersTwo'] === 'question mark') {
+                        console.log(`${chalk.blue(arr['answersTwo'])}: ${chalk.green('The answer is correct!')}`)
+                        w += 100
+                        countThree++
+                    } else {
+                        console.log(`${chalk.blue(arr['answersTwo'])}: ${chalk.red('The answer is incorrect!')}`)
+                    }
+                    if (arr['answersThree'] === 'indexof') {
+                        console.log(`${chalk.blue(arr['answersThree'])}: ${chalk.green('The answer is correct!')}`)
+                        w += 100
+                        countThree++
+                    } else {
+                        console.log(`${chalk.blue(arr['answersThree'])}: ${chalk.red('The answer is incorrect!')}`)
+                    }
+                    if (countThree < 3) {
+                        three()
+                    }
+                    if (countThree === 3) {
+                        console.log(chalk.green(`Bonus + ${w}:`), '{wallet:', `${+obj.wallet + w}}`)
+                        console.log(chalk.black.bold.bgYellow(`\n Your Account `), ` \n { name: ${obj.name}, wallet: ${+obj.wallet + w}, password: ${obj.password} }`)
+                    }
+                })
+        }
+
+        three()
+
     }
 }
+
